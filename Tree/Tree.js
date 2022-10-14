01f9962ab03f68819156abfe7a7a0c6d1aaa869e
volshebnik09 <shulgin.grigoriy@yandex.ru>
2022-10-14 13:30:46 +0300

    Tree Delete function

--- Tree/Tree.js
+++ Tree/Tree.js
@@ -50,33 +50,98 @@ export default class Tree{
 
 	toString(deep=0, root=this.root){
 		if (root === null ){
-			return 'Нет дерева'
+			return 'Нет главной вершины'
 		}
 
 		if ((root.left === null) && ((root.right === null))){
-			return ' '.repeat(deep)+ root.value
+			return '|-'.repeat(deep)+ root.value
 		}
 		if ((root.left === null) && (root.right !== null)){
-			return ' '.repeat(deep)+ root.value + '\n'+
-					' '.repeat(deep+1) + '#' + '\n' +
+			return '|-'.repeat(deep)+ root.value + '\n'+
+				'|-'.repeat(deep+1) + '#' + '\n' +
 					this.toString(deep+1, root.right)
 		}
 		if ((root.left !== null) && (root.right === null)){
-			return ' '.repeat(deep)+ root.value + '\n'+
+			return '|-'.repeat(deep)+ root.value + '\n'+
 					this.toString(deep+1, root.left) + '\n' +
-					' '.repeat(deep+1) + '#'
+				'|-'.repeat(deep+1) + '#'
 		}
 		if ((root.left !== null) && (root.right !== null)){
-			return ' '.repeat(deep)+ root.value + '\n'+
+			return '|-'.repeat(deep)+ root.value + '\n'+
 					this.toString(deep+1, root.left) + '\n' +
 					this.toString(deep+1, root.right)
 		}
 	}
 
-	deleteItem(value, root=this.root){
-		if ((root.left === null) && (root.right === null)){
-			this.root = null
-			return 'Удалён корень'
+	deleteRoot(value){
+
+		this.root = this.#deleteRoot(value)
+	}
+
+	#deleteRoot(value, root=this.root){
+
+		if (root.value === value){
+			if ((root.left === null) && (root.right === null)){
+				return null
+			}
+			if ((root.left !== null) && (root.right === null)){
+				return root.left
+			}
+			if ((root.left === null) && (root.right !== null)){
+				return root.right
+			}
+			if ((root.left !== null) && (root.right !== null)){
+				let rightMin = this.findMinValue(value, root.right)
+				root.value = rightMin
+				root.right = this.#deleteRoot(rightMin, root.right)
+				return root
+			}
+
+		}
+
+		if (root.left)
+			root.left = this.#deleteRoot(value, root.left)
+		if (root.right)
+			root.right = this.#deleteRoot(value, root.right)
+
+		return root
+	}
+
+	findMinValue(value,root=this.root){
+		if (root.left === null) {
+			return root.value
+		}
+		return this.findMinValue(value, root.left)
+	}
+
+	findRootValueContainer(value, root=this.root){
+		if (root === null) {
+			return  null
+		}
+		if ((root.right?.value === value)|| (root.left?.value === value)){
+			return root
+		}
+		if (value > root.value){
+			return this.findRootValueContainer(value, root.right)
+		}
+		if (value < root.value){
+			return this.findRootValueContainer(value, root.left)
 		}
 	}
+
+	findRoot(value, root=this.root){
+		if (root === null) {
+			return  null
+		}
+		if (value > root.value){
+			return this.findRoot(value, root.right)
+		}
+		if (value < root.value){
+			return this.findRoot(value, root.left)
+		}
+		if (root.value === value){
+			return root
+		}
+	}
+
 }
--- Tree/main.js
+++ Tree/main.js
@@ -1,11 +1,20 @@
 import Tree from "./Tree.js"
 
 let tree = new Tree()
-tree.addItem(5);
-tree.addItem(3);
-tree.addItem(6);
-tree.addItem(2);
-tree.addItem(100);
+tree.addItem(80);
+tree.addItem(81);
+tree.addItem(70);
+tree.addItem(75);
+tree.addItem(72);
+tree.addItem(71);
+tree.addItem(74);
+tree.addItem(60);
+tree.addItem(50);
+tree.addItem(55);
+
+
+console.log(tree.toString())
+console.log(tree.deleteRoot(70));
 console.log(tree.toString())
 
 /*
